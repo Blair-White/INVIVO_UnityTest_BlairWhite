509ba15f6878f08d60f8ec72c52b891ac54056f7
Blair-White <blairjwhite@hotmail.com>
2021-10-24 14:33:04 -0400

    Figured out post processing not working in build. Testing github d/l

--- INVIVO_UnityTest_BlairWhite/PC BUILD/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Blair\Documents\GitHub\INVIVO_UnityTest_BlairWhite\INVIVO_UnityTest_BlairWhite\PC BUILD\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
--- INVIVO_UnityTest_BlairWhite/PC BUILD/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_gc.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Blair\Documents\GitHub\INVIVO_UnityTest_BlairWhite\INVIVO_UnityTest_BlairWhite\PC BUILD\Il2CppOutputProject\IL2CPP\libil2cpp\gc\BoehmGC.cpp"
-#include "C:\Users\Blair\Documents\GitHub\INVIVO_UnityTest_BlairWhite\INVIVO_UnityTest_BlairWhite\PC BUILD\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GCHandle.cpp"
-#include "C:\Users\Blair\Documents\GitHub\INVIVO_UnityTest_BlairWhite\INVIVO_UnityTest_BlairWhite\PC BUILD\Il2CppOutputProject\IL2CPP\libil2cpp\gc\GarbageCollector.cpp"
-#include "C:\Users\Blair\Documents\GitHub\INVIVO_UnityTest_BlairWhite\INVIVO_UnityTest_BlairWhite\PC BUILD\Il2CppOutputProject\IL2CPP\libil2cpp\gc\NullGC.cpp"
-#include "C:\Users\Blair\Documents\GitHub\INVIVO_UnityTest_BlairWhite\INVIVO_UnityTest_BlairWhite\PC BUILD\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrier.cpp"
-#include "C:\Users\Blair\Documents\GitHub\INVIVO_UnityTest_BlairWhite\INVIVO_UnityTest_BlairWhite\PC BUILD\Il2CppOutputProject\IL2CPP\libil2cpp\gc\WriteBarrierValidation.cpp"
